45a41ed1e4e02e554a38ce8670240c6f6e106939
Dikky4217 <91036940+Dikky4217@users.noreply.github.com>
2021-10-03 17:48:58 +0530

    Add project files.

---
+++ frmMain.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace Little_Red_Store_Of_Books
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.txtBookName = new System.Windows.Forms.TextBox();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.txtFind = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.bookshopBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bookshopBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtBookName
+            // 
+            this.txtBookName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBookName.Location = new System.Drawing.Point(148, 164);
+            this.txtBookName.Name = "txtBookName";
+            this.txtBookName.Size = new System.Drawing.Size(310, 20);
+            this.txtBookName.TabIndex = 0;
+            this.txtBookName.TextChanged += new System.EventHandler(this.txtBookName_TextChanged);
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Location = new System.Drawing.Point(148, 234);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(308, 20);
+            this.txtAuthor.TabIndex = 1;
+            this.txtAuthor.TextChanged += new System.EventHandler(this.txtAuthor_TextChanged);
+            // 
+            // txtFind
+            // 
+            this.txtFind.Location = new System.Drawing.Point(542, 214);
+            this.txtFind.Name = "txtFind";
+            this.txtFind.Size = new System.Drawing.Size(334, 20);
+            this.txtFind.TabIndex = 2;
+            this.txtFind.TextChanged += new System.EventHandler(this.txtFind_TextChanged);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.AliceBlue;
+            this.btnClear.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnClear.BackgroundImage")));
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.btnClear.Location = new System.Drawing.Point(308, 277);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(150, 33);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.AliceBlue;
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.button2.Location = new System.Drawing.Point(152, 277);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 33);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "ADD BOOK";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.AliceBlue;
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.button3.Location = new System.Drawing.Point(712, 250);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 35);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "UPDATE BOOK";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.AliceBlue;
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.button4.Location = new System.Drawing.Point(556, 250);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(150, 35);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "DELETE BOOK";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.AliceBlue;
+            this.button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.button5.Location = new System.Drawing.Point(852, 415);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 55);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "EXIT BOOK STORE";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 463);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(18, 19);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // bookshopBindingSource
+            // 
+            this.bookshopBindingSource.DataMember = "Book_shop";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(939, 482);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtFind);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.txtBookName);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Book Register";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bookshopBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBookName;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.TextBox txtFind;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.BindingSource bookshopBindingSource;
+        //  private Little_redDataSetTableAdapters.Book_shopTableAdapter book_shopTableAdapter;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,107 @@
+﻿using DocumentFormat.OpenXml.Drawing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Little_Red_Store_Of_Books
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+
+            Thread t = new Thread(new ThreadStart(StartForm));
+            t.Start();
+            Thread.Sleep(5000);
+            InitializeComponent();
+            t.Abort();
+        }
+        SqlConnection con = new SqlConnection(@"Data Source=Disala;Initial Catalog=Little_red;Integrated Security=True");
+
+        public void StartForm()
+        {
+            Application.Run(new frmsplashscreen());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string bookname = txtBookName.Text;
+            string Author = txtAuthor.Text;
+            string insert_query = "insert into Book_shop values('" + bookname + "','" + Author + "')";
+            SqlCommand cmdInsert = new SqlCommand(insert_query, con);
+            con.Open();
+            cmdInsert.ExecuteNonQuery();
+            MessageBox.Show("Succsessfully Book Added!");
+            con.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string bookname = txtBookName.Text;
+            string Author = txtAuthor.Text;
+            string find = txtFind.Text;
+            string update_query = "update Book_shop Set Book_name= '" + bookname + "',Book_author='" + Author + "' where Book_name='" + find + "'";
+            SqlCommand cmdUpdate = new SqlCommand(update_query, con);
+            con.Open();
+            cmdUpdate.ExecuteNonQuery();
+            MessageBox.Show("Book Updated Succussfully!");
+            con.Close();
+
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string Find = txtFind.Text;
+            string delete_query = "delete Book_shop where Book_name='"+Find+"'";
+            SqlCommand cmdDelete = new SqlCommand(delete_query, con);
+            con.Open();
+            cmdDelete.ExecuteNonQuery();
+            MessageBox.Show("Book Deleted Succsessfully!");
+            con.Close();
+
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtAuthor.Text = "";
+            txtBookName.Text = "";
+            txtFind.Text = "";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtAuthor_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtFind_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtBookName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
